3ab74911339c007f496f471fb4b57bbfb04644ba
Shriman Sourav Sen <souravsens2010@gmail.com>
2026-03-26 02:24:37 +0530

    Files to change in staging

--- Apps/web/vite.config.js
+++ Apps/web/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'staging' ? '/web/' : '/',
+  base: '/web/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
