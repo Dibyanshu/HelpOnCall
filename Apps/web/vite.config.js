a61a2daf0368f2366bf8a9b640be3f4dbf2b8a5a
Dibyanshu Banerjee <devdarsi2darshan@gmail.com>
2026-04-16 02:56:17 +0530

    Prod cicd fix (#57)
    * Fix2
    
    * Fixed fe web
    
    ---------
    
    Co-authored-by: Dibyanshu Banerjee <dibyanshu.ndu@gmail.com>

--- Apps/web/vite.config.js
+++ Apps/web/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/web/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
